2e5d5f123bce6e85236f1024491b7a5767db0a74
Bruno Rodrigues <brunirodrigues102009@hotmail.com>
2021-11-22 23:12:08 -0300

    API no servidor

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL:"http://192.168.1.12:8000/api",
+  baseURL:"http://bruno-rodrigues.tcc.uniuv.edu.br/api",
 })
 
 api.interceptors.request.use(async (config) => {
